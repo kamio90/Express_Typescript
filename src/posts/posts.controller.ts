066a1dba21a975c892eb870184dbabebbdb7a778
Kamil Musiał <kamilfullstack@gmail.com>
2019-09-24 02:04:18 +0200

    mongodb connection

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
 import Post from './post.interface';
+import Controller from '../interfaces/controller.interface';
+import postModel from './posts.model';
 
 class PostsController {
     public path = '/posts';
     public router = express.Router();
+    private post = postModel;
 
     private posts: Post[] = [
         {
@@ -20,16 +23,54 @@ class PostsController {
     public intializeRoutes() {
         this.router.get(this.path, this.getAllPosts);
         this.router.post(this.path, this.createAPost);
+        this.router.get(`${this.path}/:id`,this.getPostById);
+        this.router.put(`${this.path}:id`, this.modifyPost);
+        this.router.delete(`${this.path}:id`, this.deletePost);
     }
 
-    getAllPosts = (request: express.Request, response: express.Response) => {
-        response.send(this.posts);
+    private getAllPosts = (request: express.Request, response: express.Response) => {
+        this.post.find()
+        .then(posts => {
+            response.send(posts)
+        });
+    }
+
+    private getPostById = (request: express.Request, response: express.Response) => {
+        const id = request.params.id;
+        this.post.findById(id)
+        .then(post => {
+            response.send(post);
+        });
+    }
+
+    private modifyPost = (request: express.Request, response: express.Response) => {
+        const id = request.params.id;
+        const postData: Post = request.body;
+        this.post.findByIdAndUpdate(id, postData, {new: true})
+        .then(post => {
+            response.send(post);
+        });
     }
     
-    createAPost = (request: express.Request, response: express.Response) => {
-        const post: Post = request.body;
-        this.posts.push(post);
-        response.send(post);
+    private createAPost = (request: express.Request, response: express.Response) => {
+        const postData: Post = request.body;
+        const createdPost = new this.post(postData);
+        createdPost.save()
+        .then(savedPost => {
+            response.send(savedPost);
+        });
+    }
+
+    private deletePost = (request: express.Request, response: express.Response) => {
+        const id = request.params.id;
+        this.post.findByIdAndDelete(id)
+        .then(successResponse => {
+            if(successResponse) {
+                response.send(200);
+            }else{
+                response.send(404);
+            }
+        });
     }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
+//mongodb+srv://<username>:<password>@typescriptapi-e2rda.mongodb.net/test?retryWrites=true&w=majority
+
 import App from './app';
 import PostController from './posts/posts.controller';
+import validateEnv from './utils/validateEnv';
+
+validateEnv();
+
+// const {MONGO_USER, MONGO_PASSWORD} = process.env;
 
 const app = new App(
     [
         new PostController()
-    ],
-    5000
+    ]
 );
 
 app.listen();
